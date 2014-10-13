f9b804e724feb6996b4d1ab056e79337043e72f9
QuadPiece <domen178@gmail.com>
2014-10-13 02:09:47 +0200

    Again, we're beyond that now

--- static/includes/opengraph.php
+++ static/includes/opengraph.php
@@ -3,22 +3,22 @@
 $site = "http://example.com";
 
 if($extension == "jpg") {
-  echo '<meta property="og:title" content="' . $filename . ' - QuadShot">
+  echo '<meta property="og:title" content="' . $filename . ' - QuadFile">
 		<meta property="og:image" content="' . $site . '/file/' . $filename . '">
 		<meta property="og:description" content="Image file uploaded to QuadFile">';
 }
 elseif($extension == "jpeg") {
-  echo '<meta property="og:title" content="' . $filename . ' - QuadShot">
+  echo '<meta property="og:title" content="' . $filename . ' - QuadFile">
     <meta property="og:image" content="' . $site . '/file/' . $filename . '">
     <meta property="og:description" content="Image file uploaded to QuadFile">';
 }
 elseif($extension == "png") {
-  echo '<meta property="og:title" content="' . $filename . ' - QuadShot">
+  echo '<meta property="og:title" content="' . $filename . ' - QuadFile">
     <meta property="og:image" content="' . $site . '/file/' . $filename . '">
     <meta property="og:description" content="Image uploaded to QuadFile">';
 }
 else {
-  echo '<meta property="og:title" content="' . $filename . ' - QuadShot">
+  echo '<meta property="og:title" content="' . $filename . ' - QuadFile">
   <meta property="og:image" content="' . $site . '/static/logo.png">
   <meta property="og:description" content="' . $extension . ' file uploaded to QuadFile">';
 }

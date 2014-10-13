3f36f2504410d15e8b4386255ed543d4f9d88407
Dominik V. Salonen <domen178@gmail.com>
2014-10-13 18:43:53 +0200

    Derp Deluxe

--- static/includes/permavideo.php
+++ static/includes/permavideo.php
@@ -2,14 +2,14 @@
 
 if($extension == "webm") {
   echo '<link href="../../static/video-js/video-js.css" rel="stylesheet">
-  <script src="../static/video-js/video.js"></script>
+  <script src="../../static/video-js/video.js"></script>
   <script>
   videojs.options.flash.swf = "../../static/video-js/video-js.swf"
   </script>';
 }
 elseif($extension == "mp4") {
   echo '<link href="../../static/video-js/video-js.css" rel="stylesheet">
-  <script src="../static/video-js/video.js"></script>
+  <script src="../../static/video-js/video.js"></script>
   <script>
   videojs.options.flash.swf = "../../static/video-js/video-js.swf"
   </script>';
